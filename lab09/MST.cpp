999487fb60ff723adcbebca1bc55bd441d0b9f88
Kylan Strope <kylanstrope@gmail.com>
2017-11-30 07:56:26 +0000

    ugh

--- lab09/MST.cpp
+++ lab09/MST.cpp
@@ -3,61 +3,86 @@
 #include <vector>
 
 using namespace std;
-int vertices = 0;
+int vertices;
 
-int extractMin(int pi[], int key[]) {
-	int index;
-	int min = INT_MAX;
+int extractMin(vector<int> valid, vector<int> key) {
+	int index = vertices - 1;
+	int minimum = INT_MAX;
 	for (int k = 0; k < vertices; k++) {
-		if (valid[j] == 0 && key[j] < min) {
-			min = key[j];
-			index = j;
+		if (valid[k] == 0 && key[k] < minimum) {
+			//cout << "ping6 index: " << index << endl;
+			minimum = key[k];
+			index = k;
 		}
 	}
-	return j;
+	//cout << "ping7 index: " << index << endl;
+	return index;
 }
 
 void MST(vector< vector<int> > graph) {
+	//cout << "pingMST1" << "vertices: " << vertices << endl;
 	int min;
-	vector<int> pi;
-	vector<int> key;
-	vector<int> valid;
+	//cout << "pingMST2" << endl;
+	vector<int> pi (vertices, 0);
+	//cout << "pingMST3" << endl;
+	vector<int> key (vertices, 0);
+	//cout << "pingMST4" << endl;
+	vector<int> valid (vertices, 0);
+	//cout << "pingMST5" << endl;
 	for (int i = 0; i < vertices; i++) {
+		//cout << "ping1" << endl;
 		key[i] = INT_MAX;
 		valid[i] = 0;
 	}
 	pi[0] = -1;
 	key[0] = 0;
-	for (int j = 0; j < vertices - 1; j++) {
-		min = extractMin(pi, key);
-	}
-	valid[min] = 1;
-	for (int k = 0; k < vertices; k++) {
-		if (graph[min][k] != 0 && valid[k] == 0 && graph[min][k] < key[k]) {
-			pi[k] = min;
-			key[k] = graph[min][k]
+	for (int j = 0; j < vertices-1; j++) {
+		//cout << "ping2 j= " << j << endl;
+		min = extractMin(valid, key);
+		valid[min] = 1;
+		for (int k = 0; k < vertices; k++) {
+			//cout << "ping3 min= " << min << " k = " << k << endl;
+			if (graph[min][k] != 0 && valid[k] == 0 && graph[min][k] < key[k]) {
+				//cout << "ping4" << endl;
+				pi[k] = min;
+				key[k] = graph[min][k];
+			}
 		}
+	}
+	for (int m = 1; m < vertices; m++) {
+		//cout << "ping5" << endl;
+		cout << pi[m] << endl;
 	}
 }
 
 int main() {
-	int vertices;
 	int edges;
 	int x, y, w;
 	cin >> vertices;
-	vector< vector<int> > graph(vertices, vector< int >(vertices, 0));
+	vector< vector<int> > graph;
+	graph.resize(vertices, vector<int>(vertices, 0));
 	for (int i = 0; i < vertices; i++) {
 		graph[i][i] = 0;
 	}
+	//for (int k = 0; k < vertices; k++) {
+	//	for (int n = 0; n < vertices; n++) {
+	//		cout << graph[k][n]  << " ";
+	//	}
+	//	cout << endl;
+	//}
 	cin >> edges;
 	for (int j = 0; j < edges; j++) {
+		//cout << "pingSeg" << endl;
 		cin >> x;
 		cin >> y;
 		cin >> w;
 		graph[x][y] = w;
+		//for (int k = 0; k < vertices; k++) {
+        		//for (int n = 0; n < vertices; n++) {
+                		//cout << graph[k][n]  << " ";
+        		//}
+        		//cout << endl;
+		//}
 	}
 	MST(graph);
-	for (int m = 1; m < vertices; m++) {
-		cout << pi[m] << endl;
-	}
 }
